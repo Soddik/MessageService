73c3abb5de9402010dcb893de75a51fdde0aefc9
Soddik <sodd86@gmail.com>
2022-09-08 03:06:48 +0300

    refactor:  +HttpStatus in ResponseEntity

--- src/main/java/com/soddik/messageapp/controller/UserController.java
+++ src/main/java/com/soddik/messageapp/controller/UserController.java
@@ -3,6 +3,7 @@ package com.soddik.messageapp.controller;
 import com.soddik.messageapp.dto.UserRequest;
 import com.soddik.messageapp.dto.UserResponse;
 import com.soddik.messageapp.service.AppUserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +21,7 @@ public class UserController {
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody UserRequest request) {
         try {
-            return ResponseEntity.ok(appUserService.create(request));
+            return new ResponseEntity<>(appUserService.create(request), HttpStatus.CREATED);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("User with name: " + request.name() + " already exist or wrong format");
         }
